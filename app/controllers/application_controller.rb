45823b70297cdd179462d0cdc88a739cd4b0a083
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-11 06:45:23 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
-class ApplicationController < ActionController::Base
-  skip_forgery_protection
+class DiceController < ApplicationController
+  def index
+  end
+
+  def roll
+    @num_dice = params[:num_dice].to_i
+    @num_sides = params[:num_sides].to_i
+    @rolls = Array.new(@num_dice) { rand(1..@num_sides) }
+  end
 end
