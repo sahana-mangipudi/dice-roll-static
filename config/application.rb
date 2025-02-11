33cf9772a6b9a02956f0f2edf1e345b9c15d7857
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-11 07:25:45 +0000

    routes established

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,40 @@
-<html>
-  <head>
-    <title>Target: Dice Roll</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+require_relative "boot"
 
-    </head> 
-    <body>
+require "rails/all"
 
-    <a href="/dice/2/6">2d6</a>
-    <a href="/dice/2/10">2d10</a>
-    <a href="/dice/1/10">1d10</a>
-    <a href="/dice/5/4">5d4</a>
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
-  
+module RailsTemplate
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework nil
+      g.factory_bot false
+      g.scaffold_stylesheet false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
+
+    config.action_controller.default_protect_from_forgery = false
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+  end
+end
